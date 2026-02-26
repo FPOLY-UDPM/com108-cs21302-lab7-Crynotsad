fe649a4469762e6380996a586fcd2bbcd61d4edc
Crynotsad <crynotsadpvp@gmail.com>
2026-02-26 11:20:50 +0700

    nguyen dinh phong

--- bai2.c
+++ bai2.c
@@ -10,3 +10,55 @@
 
 // VIẾT CODE Ở ĐÂY
 
+#include <stdio.h>
+
+int main() {
+    char username[50];
+    char password[50];
+
+    char userDung[] = "admin";
+    char passDung[] = "123456";
+
+    int i;
+    int dungUser = 1; 
+    int dungPass = 1;
+
+    printf("Nhap username: ");
+    scanf("%s", username);
+
+    printf("Nhap password: ");
+    scanf("%s", password);
+
+    // So sánh username
+    for(i = 0; userDung[i] != '\0'; i++) {
+        if(username[i] != userDung[i]) {
+            dungUser = 0;
+            break;
+        }
+    }
+
+    // Nếu độ dài khác cũng sai
+    if(username[i] != '\0') {
+        dungUser = 0;
+    }
+
+    // So sánh password
+    for(i = 0; passDung[i] != '\0'; i++) {
+        if(password[i] != passDung[i]) {
+            dungPass = 0;
+            break;
+        }
+    }
+
+    if(password[i] != '\0') {
+        dungPass = 0;
+    }
+
+    if(dungUser == 1 && dungPass == 1) {
+        printf("Dang nhap thanh cong!\n");
+    } else {
+        printf("Dang nhap khong thanh cong!\n");
+    }
+
+    return 0;
+}
--- bai3.c
+++ bai3.c
@@ -10,3 +10,66 @@
 
 // VIẾT CODE Ở ĐÂY
 
+#include <stdio.h>
+
+int main() {
+    char str[5][100];
+    char temp[100];
+    int i, j, k;
+
+    // Nhập 5 chuỗi
+    for(i = 0; i < 5; i++) {
+        printf("Nhap chuoi thu %d: ", i + 1);
+        scanf("%s", str[i]);
+    }
+
+    // Sắp xếp
+    for(i = 0; i < 4; i++) {
+        for(j = i + 1; j < 5; j++) {
+
+            k = 0;
+
+            // So sánh từng ký tự
+            while(str[i][k] != '\0' && str[j][k] != '\0') {
+                if(str[i][k] > str[j][k]) {
+                    
+                    // Đổi chỗ hai chuỗi
+                    int t = 0;
+                    while(str[i][t] != '\0') {
+                        temp[t] = str[i][t];
+                        t++;
+                    }
+                    temp[t] = '\0';
+
+                    t = 0;
+                    while(str[j][t] != '\0') {
+                        str[i][t] = str[j][t];
+                        t++;
+                    }
+                    str[i][t] = '\0';
+
+                    t = 0;
+                    while(temp[t] != '\0') {
+                        str[j][t] = temp[t];
+                        t++;
+                    }
+                    str[j][t] = '\0';
+
+                    break;
+                }
+                else if(str[i][k] < str[j][k]) {
+                    break;
+                }
+                k++;
+            }
+        }
+    }
+
+    // In kết quả
+    printf("\nDanh sach sau khi sap xep:\n");
+    for(i = 0; i < 5; i++) {
+        printf("%s\n", str[i]);
+    }
+
+    return 0;
+}
